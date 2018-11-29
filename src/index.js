2adaf1c75d4b77010b60381ddf9ca6df5c5507b0
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2018-11-29 13:38:59 +0530

    Adding some co-ordinates to show Dhruv

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const coords=[[1,1],[1,2],[1,3]];
+const coords=[[1,1],[1,2],[1,3], [2,3], [4,4]];
 ReactDOM.render(<App gen={coords}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
