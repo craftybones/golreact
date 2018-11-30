818ccdeceab1d5b149a48fb2e1b1b2d87aa0f3aa
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2018-11-30 10:46:29 +0530

    add home page to package json now accepts a slug instead of url

--- addHomepageToPackage.js
+++ addHomepageToPackage.js
@@ -1,5 +1,7 @@
 const fs=require('fs');
 const pkg=require('./package.json');
-pkg.homepage=process.env.GITHUB_HOMEPAGE;
+const slug=process.env.GITHUB_HOMEPAGE || process.env.TRAVIS_REPO_SLUG;
+let repo=slug.split(/\//).slice(-1)[0];
+pkg.homepage=`https://step-tw.github.io/${repo}`
 
 fs.writeFileSync("package.json",JSON.stringify(pkg),"utf8");
